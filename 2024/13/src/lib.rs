00000009fd44b90bb90bb2ce1f2bf69dd982022f
Soorria Saruva <soorria.ss@gmail.com>
2024-12-13 22:25:08 +1100

    chore: clean up day 13

--- 2024/13/src/lib.rs
+++ 2024/13/src/lib.rs
@@ -13,77 +13,59 @@ struct Machine {
     prize: (f64, f64),
 }
 
+fn solve_machine(machine: Machine) -> Option<f64> {
+    let Machine { a, b, prize } = machine;
+
+    let b_times = (prize.1 * a.0 - prize.0 * a.1) / (b.1 * a.0 - a.1 * b.0);
+    let a_times = (prize.0 - b.0 * b_times) / (a.0);
+
+    if a_times.fract() != 0.0 || b_times.fract() != 0.0 {
+        None
+    } else {
+        Some(3.0 * a_times + b_times)
+    }
+}
+
+fn parse_machine(machine_str: &str) -> Machine {
+    let parts = machine_str
+        .lines()
+        .map(|line| {
+            let (_, line) = line.split_once(": ").unwrap();
+            let nums = line
+                .split(", ")
+                .map(|coord| coord[2..].parse::<f64>().unwrap())
+                .collect::<Vec<_>>();
+
+            (nums[0], nums[1])
+        })
+        .collect::<Vec<_>>();
+
+    return Machine {
+        a: parts[0],
+        b: parts[1],
+        prize: parts[2],
+    };
+}
+
 pub fn solve_a(input: String) -> i64 {
     input
         .split("\n\n")
-        .map(|machine_str| {
-            let parts = machine_str
-                .lines()
-                .map(|line| {
-                    let (_, line) = line.split_once(": ").unwrap();
-                    let nums = line
-                        .split(", ")
-                        .map(|coord| coord[2..].parse::<f64>().unwrap())
-                        .collect::<Vec<_>>();
-
-                    (nums[0], nums[1])
-                })
-                .collect::<Vec<_>>();
-
-            return Machine {
-                a: parts[0],
-                b: parts[1],
-                prize: parts[2],
-            };
-        })
-        .filter_map(|Machine { a, b, prize }| {
-            let b_times = (prize.1 * a.0 - prize.0 * a.1) / (b.1 * a.0 - a.1 * b.0);
-            let a_times = (prize.0 - b.0 * b_times) / (a.0);
-
-            if a_times.fract() != 0.0 || b_times.fract() != 0.0 {
-                None
-            } else {
-                Some(3.0 * a_times + b_times)
-            }
-        })
+        .map(parse_machine)
+        .filter_map(solve_machine)
         .sum::<f64>() as i64
 }
 
-const B_INC: f64 = 10000000000000.0;
+const PART_B_INC: f64 = 10000000000000.0;
 
 pub fn solve_b(input: String) -> i64 {
     input
         .split("\n\n")
-        .map(|machine_str| {
-            let parts = machine_str
-                .lines()
-                .map(|line| {
-                    let (_, line) = line.split_once(": ").unwrap();
-                    let nums = line
-                        .split(", ")
-                        .map(|coord| coord[2..].parse::<f64>().unwrap())
-                        .collect::<Vec<_>>();
-
-                    (nums[0], nums[1])
-                })
-                .collect::<Vec<_>>();
-
-            return Machine {
-                a: parts[0],
-                b: parts[1],
-                prize: (parts[2].0 + B_INC, parts[2].1 + B_INC),
-            };
+        .map(parse_machine)
+        .map(|machine| Machine {
+            prize: (machine.prize.0 + PART_B_INC, machine.prize.1 + PART_B_INC),
+            ..machine
         })
-        .filter_map(|Machine { a, b, prize }| {
-            let b_times = (prize.1 * a.0 - prize.0 * a.1) / (b.1 * a.0 - a.1 * b.0);
-            let a_times = (prize.0 - b.0 * b_times) / (a.0);
-
-            if a_times.fract() != 0.0 || b_times.fract() != 0.0 {
-                None
-            } else {
-                Some(3.0 * a_times + b_times)
-            }
-        })
+        .filter_map(solve_machine)
         .sum::<f64>() as i64
 }
 
